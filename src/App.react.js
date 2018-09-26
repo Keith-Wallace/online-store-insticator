e610c3d99987244de1b8a3ec8719a9691b286bcb
Keith Wallace <keith.t.wallace@gmail.com>
2018-09-26 18:06:41 -0400

    Wire up the `App.react.js` entry point with json data

--- src/App.react.js
+++ src/App.react.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {Component}  from 'react'
 import ReactDOM from 'react-dom'
 
+import StoreDashboard from './components/StoreDashboard.react'
+import STORE_DATA from './data/store_items.json'
 
-const App = () => {
-  return (
-    <div>
-      <p>React here!</p>
-    </div>
-  )
+import './globalStyles.scss'
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      storeData: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      storeData: STORE_DATA,
+      loading: false
+    })
+  }
+
+  render() { return <StoreDashboard {...this.state} /> }
 }
-
-export default App
 
 ReactDOM.render(
   <App />,
